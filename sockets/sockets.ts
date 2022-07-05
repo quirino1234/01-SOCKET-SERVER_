15030116c0bfca1d561c589bd5b786f14692a297
Eleazar <eleazar.cortezquirino@utxicotepec.edu.mx>
2022-07-05 14:32:21 -0500

    Prueba del servidor remoto, recepcion de mensajes

---
+++ sockets/sockets.ts
@@ -0,0 +1,18 @@
+import { Socket } from "socket.io";
+import socketIO from 'socket.io';
+
+export const desconectar = ( cliente: Socket) =>{
+    cliente.on('disconnect', ()=>
+    {
+        console.log('Cliente Desconectado');
+    })
+}
+export const mensaje = (cliente:Socket, io: socketIO.Server)=>{
+    cliente.on('mensaje', (payload:{de: string, cuerpo:string}) => {
+       
+        console.log('Mensaje',payload);
+
+        io.emit('mensaje-nuevo', payload);
+
+    })
+}
